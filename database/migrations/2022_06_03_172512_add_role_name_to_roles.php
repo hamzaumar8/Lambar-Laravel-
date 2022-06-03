27e0116ce6659d143811d16c5e83707b6dcd5eff
hamza umar <humar6078@gmail.com>
2022-06-03 17:30:30 +0000

    roles init

--- database/migrations/2022_06_03_172512_add_role_name_to_roles.php
+++ database/migrations/2022_06_03_172512_add_role_name_to_roles.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class AddRoleNameToRoles extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::table('roles', function (Blueprint $table) {
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +25,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('roles', function (Blueprint $table) {
+            $table->string('name');
+        });
     }
 }
